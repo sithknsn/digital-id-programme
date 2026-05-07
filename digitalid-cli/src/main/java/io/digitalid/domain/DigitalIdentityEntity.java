5cf0d390fd496f23e102afa32896e14de13bee18
Sithaarthan Kanasan <sithknsn@amazon.com>
2026-05-07 16:56:05 +0100

    Established identity attributes and status for domain (#1) (#2)

---
+++ digitalid-cli/src/main/java/io/digitalid/domain/DigitalIdentityEntity.java
@@ -0,0 +1,40 @@
+package io.digitalid.domain;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public abstract class DigitalIdentityEntity {
+    private final UUID digitalId;
+    private final LocalDateTime dateCreated;
+    private final LocalDate dateOfBirth;
+    private final String nationality;
+    private String firstName;
+    private String lastName;
+    private UserStatus status;
+    private LocalDateTime lastModified;
+
+    public DigitalIdentityEntity(LocalDate dateOfBirth, String nationality) {
+        this.digitalId = UUID.randomUUID();
+        this.dateCreated = LocalDateTime.now();
+        this.dateOfBirth = dateOfBirth;
+        this.nationality = nationality;
+        this.status = UserStatus.UNVERIFIED;
+    }
+
+    public UUID getDigitalId() { return digitalId; }
+    public LocalDateTime getDateCreated() { return dateCreated; }
+    public LocalDate getDateOfBirth() { return dateOfBirth; }
+    public String getNationality() { return nationality; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public UserStatus getStatus() { return status; }
+    protected void setStatus(UserStatus status) { this.status = status; }
+
+    public LocalDateTime getLastModified() { return lastModified; }
+    public void setLastModified(LocalDateTime lastModified) { this.lastModified = lastModified; }
+}
---
+++ digitalid-cli/src/main/java/io/digitalid/domain/UserStatus.java
@@ -0,0 +1,9 @@
+package io.digitalid.domain;
+
+public enum UserStatus {
+    EXPIRED,
+    ACTIVE,
+    REVOKED,
+    UNVERIFIED
+
+}
